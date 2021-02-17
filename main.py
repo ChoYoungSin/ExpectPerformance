c547e9aea90670e1ab283641d0b38465d8947d90
영시니 <jysboy03@gmail.com>
2021-02-17 22:34:45 +0900

    .py 파일들 여러개로 나눔

--- main.py
+++ main.py
@@ -13,10 +13,9 @@ if __name__ == "__main__":
         diffList = diffStockList(preDisclosure, disclosure)
 
 
-        '''
-        expect   : 잠정 실적 종목 정보들 param : [종목 코드, 종목명, rcept_no, corp_code]
-        thisTerm : 정기 공시 종목 정보들 param : [종목 코드, 종목명, rcept_no, corp_code]
-        '''
+        # expect   : 잠정 실적 종목 정보들 param : [종목 코드, 종목명, rcept_no, corp_code]
+        # thisTerm : 정기 공시 종목 정보들 param : [종목 코드, 종목명, rcept_no, corp_code]
+
         expect, thisTerm = getRceptNum(diffList)
         result = crawlingRcept(expect)  # result에 신규 잠정실적 크롤링 결과 있음
         output = df(data=result['data'])
@@ -31,4 +30,10 @@ if __name__ == "__main__":
             print(ep['종목명'].ljust(15, ' '), "매출액 비교", ep['매출액 성장률'], '\t', yoy)
         print('Done')
         time.sleep(2.5)
+        # 개장 시간 : 380분, 22800초
+        # 하루에 1만회 조회 가능
+        # 22800 / 10000 = 2.28
+        # 2.28초에 1회 조희시 개장 시간동안 유지, 넉넉히 2.5초당 한번
+        # notice : get_finance_info는 항상 실행되지 않고 하루에 발행되는 정기 공시 만큼 실행되기 때문에 대세에 지장 없음.
+
         break
